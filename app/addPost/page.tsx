d53a67f6284b43cef4a260d9b98be1b3aee0b48a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-25 16:14:03 +0300

    Created the addPost page

---
+++ app/addPost/page.tsx
@@ -0,0 +1,9 @@
+type Props = {}
+
+const AddPostPage = (props: Props) => {
+	return <main className="min-h-screen">
+        <h1 className="p-5 uppercase text-3xl">Add Post</h1>
+    </main>
+}
+
+export default AddPostPage
