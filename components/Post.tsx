f29db91d61340b37feb6486154caf9c29906c46d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-25 16:08:38 +0300

    Added some brand new styles to the post

--- components/Post.tsx
+++ components/Post.tsx
@@ -18,10 +18,13 @@ const Post = ({
 	title
 }: Props) => {
 	return (
-		<div className='border border-white rounded-md p-5'>
-			<h3>{authorName}</h3>
-			<h4>{title}</h4>
-			<p>{content}</p>
+		<div className='border border-white rounded-md p-5 w-[30vw] m-5'>
+			<div className='flex flex-col items-center'>
+				<h3 className='text-3xl uppercase font-bold'>{title}</h3>
+				<h4 className='text-2xl font-semibold'>{content}</h4>
+			</div>
+
+			<p className='text-sm text-right'> - {authorName}</p>
 		</div>
 	)
 }
