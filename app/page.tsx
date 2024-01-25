c600dfda8d9a70794ca8ceb1aece32fb8531a10b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-25 15:54:11 +0300

    Added a function to fetch posts from the database

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,28 @@
-export default function Home() {
-  return (
-    <main className="min-h-screen">
-      <h1 className="text-3xl uppercase py-5 px-10 font-semibold">Feed</h1>
-    </main>
-  )
+import { prisma } from '../lib/prisma'
+
+async function getPosts() {
+	const posts = await prisma.post.findMany({
+		where: { published: true },
+		include: {
+			author: {
+				select: { name: true }
+			}
+		}
+	})
+
+  return posts
+}
+
+export default async function Home() {
+  const posts = await getPosts()
+
+  console.log(posts)
+
+	return (
+		<main className='min-h-screen'>
+			<h1 className='text-3xl uppercase py-5 px-10 font-semibold'>
+				Feed
+			</h1>
+		</main>
+	)
 }
