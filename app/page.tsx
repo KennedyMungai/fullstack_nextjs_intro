334e22a2cdf58a3726874fced95ccf8d61a1b169
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-25 15:49:54 +0300

    Added some markup and styles to the index page of the application

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <main className="min-h-screen"></main>
+    <main className="min-h-screen">
+      <h1 className="text-3xl uppercase py-5 px-10 font-semibold">Feed</h1>
+    </main>
   )
 }
